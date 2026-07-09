1a78e9e8973de175743689b3ee9ce070d05e2c6a
Nicholas Williams <nickwill820@gmail.com>
2026-07-09 00:21:42 -0400

    feat: Setup Fabric for Minecraft 1.21.11

---
+++ src/client/java/pro/piconico/automata/client/AutomataClient.java
@@ -0,0 +1,10 @@
+package pro.piconico.automata.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutomataClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
